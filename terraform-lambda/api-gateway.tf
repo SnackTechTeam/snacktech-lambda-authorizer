221a23af6bcddc0e12730a9c19ee87573b5a225c
Adriano Costa <adriano.dmcosta@gmail.com>
2024-12-15 19:17:56 -0300

    exigir cpf na request pro api gateway

--- terraform-lambda/api-gateway.tf
+++ terraform-lambda/api-gateway.tf
@@ -7,7 +7,7 @@
 
 # Create the REST API Gateway
 resource "aws_api_gateway_rest_api" "api" {
-  name = "snacktech-api"
+  name = "${var.projectName}-api"
   
   endpoint_configuration {
     types = ["REGIONAL"]
@@ -16,7 +16,7 @@ resource "aws_api_gateway_rest_api" "api" {
 
 # Create the Lambda authorizer
 resource "aws_api_gateway_authorizer" "lambda_authorizer" {
-  name                   = "snacktech-authorizer"
+  name                   = "${var.projectName}-authorizer"
   rest_api_id            = aws_api_gateway_rest_api.api.id
   authorizer_uri         = aws_lambda_function.authorizer.invoke_arn
   authorizer_credentials = data.aws_iam_role.labrole.arn
@@ -40,7 +40,8 @@ resource "aws_api_gateway_method" "proxy" {
   authorizer_id = aws_api_gateway_authorizer.lambda_authorizer.id
 
   request_parameters = {
-    "method.request.path.proxy" = true
+    "method.request.path.proxy" = true,
+    "method.request.header.cpf" = true
   }
 }
 
